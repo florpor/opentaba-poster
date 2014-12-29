3732f3cf717f93911df5afb62b2bab57814a1088
florpor <florist.powers@gmail.com>
2014-12-29 10:04:18 +0200

    poster service shouldn't know about plans. also poster id now in db

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import logging
 from json import loads
 from threading import Thread
 from Queue import Queue
+import pymongo
 
 from flask import Flask, request, abort, make_response
 import bitly_api
 from facepy import GraphAPI
 from twitter import *
+
+from conn import db
 
 logging.basicConfig(format='%(asctime)-15s %(name)s %(levelname)s %(message)s', level=logging.WARNING)
 log = logging.getLogger(__name__)
@@ -24,45 +27,58 @@ def post_worker():
         # this will block the thread indefinetly until an item is popped
         post_params = post_queue.get()
         
-        plan = loads(post_params['plan'])
+        post = loads(post_params['post'])
         
-        # if bitly access token is defined shorten the link
-        if 'BITLY_TOKEN' in os.environ.keys():
-            try:
-                b = bitly_api.Connection(access_token=os.environ['BITLY_TOKEN'])
-                shortie = b.shorten(plan['url'])
-                plan['url'] = shortie['url']
-            except Exception, e:
-                log.exception('Could not shorten the link using bit.ly - %s', e)
+        # get the poster's data from our db
+        poster_data = db.posters.findOne({'id': post['poster_id']})
         
-        # facebook-needed params
-        if all(param in post_params.keys() for param in ['fb_tok', 'fb_page']):
-            try:
-                graph = GraphAPI(post_params['fb_tok'])
-                graph.post(
-                    path = 'v2.2/%s/feed' % post_params['fb_page'],
-                    message = '%s: %s %s' % (plan['title'], plan['content'], plan['url']),
-                    retry = 10
-                )
-            except Exception, e:
-                log.exception('Could not post new plan to facebook page - %s', e)
-    
-        # twitter-needed params
-        if all(param in post_params.keys() for param in ['tw_tok', 'tw_tsec', 'tw_con', 'tw_csec']):
-            try:
-                tweet_content = '%s: %s' % (plan['title'], plan['content'])
+        if poster_data is None:
+            log.exception('poster_id not valid')
+        else:
+            # get post details
+            url = (post['url'] if 'url' in post.keys() else '')
+            title = (post['title'] if 'title' in post.keys() else '')
+            content = (post['content'] if 'content' in post.keys() else '')
             
-                # shorten our content - max size should be 118, not including the link which will be shortened by twitter if bit.ly is not enabled
-                if len(tweet_content) > 118:
-                    tweet = '%s... %s' % (tweet_content[0:114], plan['url'])
-                else:
-                    tweet = '%s %s' % (tweet_content, plan['url'])
-            
-                t = Twitter(auth=OAuth(consumer_key=post_params['tw_con'], consumer_secret=post_params['tw_csec'], token=post_params['tw_tok'], token_secret=post_params['tw_tsec']))
-                t.statuses.update(status=tweet)
-            except Exception, e:
-                log.exception('Could not post new plan to twitter feed - %s', e)
+            # if bitly access token is defined shorten the link
+            if len(url) > 0 and 'BITLY_TOKEN' in os.environ.keys():
+                try:
+                    b = bitly_api.Connection(access_token=os.environ['BITLY_TOKEN'])
+                    shortie = b.shorten(url)
+                    url = shortie['url']
+                except Exception, e:
+                    log.exception('Could not shorten the link using bit.ly - %s', e)
+                
+            # facebook-needed params
+            if all(param in poster_data.keys() for param in ['fb_tok', 'fb_page']):
+                try:
+                    graph = GraphAPI(poster_data['fb_tok'])
+                    graph.post(
+                        path = 'v2.2/%s/feed' % poster_data['fb_page'],
+                        message = '%s: %s %s' % (title, content, url),
+                        retry = 10
+                    )
+                except Exception, e:
+                    log.exception('Could not post new plan to facebook page - %s', e)
+                
+            # twitter-needed params
+            if all(param in poster_data.keys() for param in ['tw_tok', 'tw_tsec', 'tw_con', 'tw_csec']):
+                try:
+                    tweet_content = '%s: %s' % (title, content)
+                    
+                    # shorten our content - max size should be 118, not including the link which will be
+                    # shortened by twitter if bit.ly is not enabled
+                    if len(tweet_content) > 118:
+                        tweet = '%s... %s' % (tweet_content[0:114], url)
+                    else:
+                        tweet = '%s %s' % (tweet_content, url)
+                    
+                    t = Twitter(auth=OAuth(consumer_key=poster_data['tw_con'], consumer_secret=poster_data['tw_csec'], token=poster_data['tw_tok'], token_secret=poster_data['tw_tsec']))
+                    t.statuses.update(status=tweet)
+                except Exception, e:
+                    log.exception('Could not post new plan to twitter feed - %s', e)
         
+        # done with this task. release
         post_queue.task_done()
 
 
@@ -74,8 +90,8 @@ def post_route():
     post plan data to facebook, twitter or both
     """
     
-    # check if post is given, otherwise bad request
-    if 'plan' not in request.form.keys():
+    # check if poster_id is given, otherwise bad request
+    if 'poster_id' not in request.form.keys():
         abort(400)
     
     # queue the request
